976aa5cb4a2709e476defa52b316a4c307ab143c
Dindin Abduloh <dindin.abduloh@roketin.com>
2022-10-12 13:45:54 +0700

    update

--- src/axios/api.js
+++ src/axios/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
   headers: { 'Access-Control-Allow-Origin': 'https://testingbengkel.herokuapp.com' },
-  baseURL: 'http://ec2-43-206-107-140.ap-northeast-1.compute.amazonaws.com/digibengkel/api/v1/'
+  baseURL: 'https://testingbengkel.herokuapp.com/api/v1/'
 })
